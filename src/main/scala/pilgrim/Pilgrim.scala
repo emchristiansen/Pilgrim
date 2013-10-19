9524ed910e1dd6d396cc261b3d59d4b247e8830a
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-10-18 21:39:52 -0700

    simple working example

--- src/main/scala/pilgrim/Pilgrim.scala
+++ src/main/scala/pilgrim/Pilgrim.scala
@@ -18,9 +18,22 @@ class Conf(args: Seq[String]) extends ScallopConf(args) {
 }
 
 object Pilgrim {
+  /**
+   * This unfortunate bit of magic is a workaround for an
+   * apparent concurrency bug in SLF4J.
+   */
+  def initializeLogger() {
+    val classLoader = this.getClass.getClassLoader
+    classLoader.loadClass("org.slf4j.LoggerFactory").
+      getMethod("getLogger", classLoader.loadClass("java.lang.String")).
+      invoke(null, "ROOT")
+  }
+
   def main(unparsedArgs: Array[String]) {
+    initializeLogger()
+    
     val args = new Conf(unparsedArgs)
-//    println(args.summary)
+    //    println(args.summary)
 
     def splitString(string: String) =
       string.split(" ").map(_.trim).filterNot(_.isEmpty).toList
@@ -36,7 +49,7 @@ object Pilgrim {
     val taskClassName :: taskOptions = splitString(args.task())
     val taskMainClass =
       Class.forName(taskClassName).newInstance.asInstanceOf[Task]
-    
+
     loadOpenCV
     taskMainClass.run(taskOptions)
   }
--- src/main/scala/tasks/SimpleOxford.scala
+++ src/main/scala/tasks/SimpleOxford.scala
@@ -12,25 +12,65 @@ import scala.pickling.binary._
 import st.sparse.billy.detectors.BoundedDetector
 import shapeless._
 import shapeless.poly._
+import scala.reflect.runtime._
 
 class SimpleOxford extends Task {
   override def run(unparsedArgs: Seq[String])(
     implicit runtimeConfig: RuntimeConfig) {
     require(unparsedArgs.isEmpty)
+
+    val cm = universe.runtimeMirror(getClass.getClassLoader)
+    import scala.tools.reflect.ToolBox
+    val tb = cm.mkToolBox()
+
+    def typeName[A: FastTypeTag]: String = 
+      implicitly[FastTypeTag[A]].tpe.toString
+      
+    def tn[A: FastTypeTag] = typeName[A]
     
-    val matchers = VectorMatcher.L1 :: VectorMatcher.L2 :: HNil
+    def constructorName[A: FastTypeTag]: String =
+      typeName[A].takeWhile(_ != '[')
+      
+    def cn[A: FastTypeTag] = constructorName
     
-    matchers map identity
+    def objectName[A: FastTypeTag]: String =
+      typeName[A].replace(".type", "")
+      
+    def on[A: FastTypeTag]: String = objectName[A]
     
-    val experiments = (2 to 6) map { otherImage =>
-      Oxford(
-        "boat",
-        otherImage,
-        BoundedDetector(OpenCVDetector.FAST, 100),
-        OpenCVExtractor.SIFT,
-        VectorMatcher.L2): Experiment
-    }
-    
+    def evalExperiments(
+      imageClasses: Seq[String],
+      otherImages: Seq[Int],
+      detectorStrings: Seq[String],
+      extractorStrings: Seq[String],
+      matcherStrings: Seq[String]): Seq[Experiment] =
+      for (
+        imageClass <- imageClasses;
+        otherImage <- otherImages;
+        detectorString <- detectorStrings;
+        extractorString <- extractorStrings;
+        matcherString <- matcherStrings
+      ) yield {
+        val code = s"""        
+${cn[Oxford[_, _, _, _]]}(
+  "$imageClass", 
+  $otherImage, 
+  $detectorString, 
+  $extractorString, 
+  $matcherString): ${cn[Experiment]}    	
+"""
+        println(code)
+
+        tb.eval(tb.parse(code)).asInstanceOf[Experiment]
+      }
+
+    val experiments = evalExperiments(
+      Seq("boat"),
+      2 to 6,
+      Seq(s"${cn[BoundedDetector[_]]}(${on[OpenCVDetector.FAST.type]}, 100)"),
+      Seq(on[OpenCVExtractor.SIFT.type], on[OpenCVExtractor.SURF.type]),
+      Seq(on[VectorMatcher.L1.type], on[VectorMatcher.L2.type]))
+
     val results = experiments.par.map(_.run).toIndexedSeq
     results foreach (println)
 
